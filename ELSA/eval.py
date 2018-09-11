72c437a14bb14d969b03221022d57afef83ed140
y4cj4sul3 <hl6540@gmail.com>
2018-09-11 09:59:22 +0800

    improve visualize

--- ELSA/eval.py
+++ ELSA/eval.py
@@ -86,47 +86,54 @@ with tf.Session() as sess:
     print(np.shape(_hidden))
 
     # Plot Trajectory
+    base_id = 0
+    plt_sz = 1
+    plt_range = [-plt_sz, plt_sz, -plt_sz, plt_sz]
 
     # compare input output
     for i in range(max_seq_len-1):
-      plt.plot(_input[0, i:i+2, 0], _input[0, i:i+2, 1], c=[0, 1, 0, 0.5])
-      plt.plot(_output[0, i:i+2, 0], _output[0, i:i+2, 1], c=[1, 0, 0, float(i)/float(max_seq_len)])
+      time_factor = float(i)/float(max_seq_len)
+      plt.plot(_input[base_id, i:i+2, 0], _input[base_id, i:i+2, 1], c=[0, 0.5*(1-time_factor), 1, 0.5])
+      plt.plot(_output[base_id, i:i+2, 0], _output[base_id, i:i+2, 1], c=[1, 0.5*(1-time_factor), 0, 0.5])
       #plt.arrow(_output[0, i, 0], _output[0, i, 1], 0.1*_output[0, i, 2], 0.1*_output[0, i, 3])
 
-    plt.axis([-10, 10, -10, 10])
+    plt.axis(plt_range)
     plt.show()
-
+    
     # lstm
     if len(np.shape(_hidden)) == 3:
       _hidden = np.reshape(np.stack(_hidden, axis=1), (-1, 2*hidden_size))
 
     # compare latent code
     max_d = 0
+    h_dists = []
     for i in range(batch_size):
-      dist = mse(_hidden[0], _hidden[i])
+      dist = mse(_hidden[base_id], _hidden[i])
       if dist > max_d:
         max_d = dist
+      h_dists.append(dist)
+    print('largest distance(aganist base): {}'.format(max_d))
 
     for i in range(batch_size):
-      dist = mse(_hidden[0], _hidden[i]) 
-      plt.plot(_output[i, :, 0], _output[i, :, 1], c=[(dist/max_d), 0, 0.5, 1])
+      #dist = mse(_hidden[0], _hidden[i]) 
+      plt.plot(_output[i, :, 0], _output[i, :, 1], c=[(h_dists[i]/max_d), 0, 0.5, 1])
       #plt.plot(data[i, :, 0], data[i, :, 1], c=[(dist/max_d), 0, 0.5, 1])
     
-    plt.plot(_input[0, :, 0], _input[0, :, 1], c=[1, 1, 0, 1])
+    plt.plot(_input[base_id, :, 0], _input[base_id, :, 1], c=[1, 1, 0, 1])
     #plt.plot(data[0, :, 0], data[0, :, 1], c=[1, 1, 0, 1])
 
-    plt.axis([-10, 10, -10, 10])
+    plt.axis(plt_range)
 
     plt.show()
 
     # plot original trajectory
     for i in range(batch_size):
-        dist = mse(_hidden[0], _hidden[i])
-        plt.plot(_input[i, :, 0], _input[i, :, 1], c=[(dist/max_d), 0, 0.5, 1])
+        #dist = mse(_hidden[0], _hidden[i])
+        plt.plot(_input[i, :, 0], _input[i, :, 1], c=[(h_dists[i]/max_d), 0, 0.5, 1])
 
-    plt.plot(_input[0, :, 0], _input[0, :, 1], c=[1, 1, 0, 1])
+    plt.plot(_input[base_id, :, 0], _input[base_id, :, 1], c=[1, 1, 0, 1])
 
-    plt.axis([-10, 10, -10, 10])
+    plt.axis(plt_range)
 
     plt.show()
 
